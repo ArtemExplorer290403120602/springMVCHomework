acf2c0d90d59983c2d0d6f507bd10c84da7348bd
User <artem.zakrzhevski@mail.ru>
2024-04-23 01:20:52 +0300

    Add main function about project

---
+++ src/main/java/com/home/service/AnimalService.java
@@ -0,0 +1,9 @@
+package com.home.service;
+
+import com.home.repository.AnimalRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AnimalService {
+    private AnimalRepository animalRepository;
+}
